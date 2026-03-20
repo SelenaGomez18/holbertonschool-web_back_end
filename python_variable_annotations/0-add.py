12f122c7311a10bbd68a90abd1296992d371aa6a
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-03-19 20:27:07 -0500

    Add answer task 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 def add(a: float, b: float) -> float:
+    
+     """
+    Returns the sum of two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
