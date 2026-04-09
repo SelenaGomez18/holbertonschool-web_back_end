2d7af4c59164aa8955917a2f0ab1e53c13c094ef
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-04-09 18:35:09 -0500

    Create a new function for cut of start and end

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Simple pagination"""
 
+
 import csv
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return start and end index for pagination"""
-    start = (page -1) * page_size
+    start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
+
 
 class Server:
     """Server class to paginate a database"""
@@ -28,8 +30,9 @@ class Server:
             self.__dataset = dataset[1:]
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size : int = 10) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return a page of data"""
+
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
