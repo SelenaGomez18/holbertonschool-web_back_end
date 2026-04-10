4527b6cbac79de2691db3286d85b825d3b19b84b
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-04-09 19:03:47 -0500

    Create a new function for cut of start and end

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""Deletion-resilient pagination"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class"""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Load dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Indexed dataset"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """Return deletion-resilient page"""
+        dataset = self.indexed_dataset()
+
+        assert isinstance(index, int) and index >= 0 and index < len(dataset)
+
+        data = []
+        current = index
+
+        # collect page_size items skipping deleted indexes
+        while len(data) < page_size and current < len(dataset):
+            if current in dataset:
+                data.append(dataset[current])
+            current += 1
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": len(data),
+            "next_index": current
+        }
