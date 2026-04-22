291df97035c4ae9bb5de47a5eaece841a7140eb3
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-04-21 19:29:36 -0500

    Add a script to attend a place like this at some point Implement a class named ClassRoom

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,36 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter y Setter para amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  // Getter y Setter para currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  // Método para mostrar el precio completo
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Método estático para convertir precios
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
