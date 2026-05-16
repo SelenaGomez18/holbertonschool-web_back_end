8e5bbaca3016daaa809b2a49b8bbefc6277fe5f6
SelenaGomez18 <selenagomezrueda045@gmail.com>
2026-05-16 00:01:32 -0500

    Create a program named 1-stdin.js that will be executed through command line

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,10 +2,12 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
+// Handle user input
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.trim()}\n`);
 });
 
+// Handle program termination
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
